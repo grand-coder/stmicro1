bd38bd0e7bb9f2ac2833688dfcacfa68b5fef8ee
grand-coder <37803705+grand-coder@users.noreply.github.com>
2019-11-18 13:47:45 +0800

    Add files via upload

--- 2_猜英文.py
+++ 2_猜英文.py
@@ -1,7 +1,25 @@
+import random
 from random_words import RandomWords
 rw = RandomWords()
-word = rw.random_word()
-print(word)
 
+total = 0
+while True:
+    word = rw.random_word()
+    # 第三個:2
+    # [0:2] + "_" + [3:]
+    i = random.randint(0, len(word)-1)
+    guess = word[:i] + "_" + word[i+1:]
+    g = input(guess + "是什麼?")
+
+    if g == word[i]:
+        print("猜對了")
+        total = total + 1
+    else:
+        print("猜錯了")
+        print("正確答案:", word)
+
+    if total >= 5:
+        print("今天結束了")
+        break
 
 
